7d97e12022a4b675c87130d0c22dae6573f15bd2
brandonbews <b@bews.io>
2024-12-20 11:15:38 -0800

    Fix missing tag name

--- src/components/Hit.jsx
+++ src/components/Hit.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Hit({ hit, highlight: Highlight }) {
-  const [notification, setNotification] = useState("");
+  const [notification, setNotification] = useState('')
 
   const handleAddToCart = () => {
     // Retireve current cart from local storage
-    const currentCart = JSON.parse(localStorage.getItem("cart")) || [];
+    const currentCart = JSON.parse(localStorage.getItem('cart')) || []
 
     // Check if the item is already in the cart
-    const itemInCart = currentCart.find(
-      (item) => item.objectID === hit.objectID
-    );
+    const itemInCart = currentCart.find((item) => item.objectID === hit.objectID)
     // If the item is already in the cart, increase the quantity
     if (itemInCart) {
-      itemInCart.quantity += 1;
+      itemInCart.quantity += 1
     }
     // If the item is not in the cart, add it
 
@@ -23,67 +21,51 @@ function Hit({ hit, highlight: Highlight }) {
     //
     else {
       currentCart.push({
-        objectID: hit["objectID"],
-        name: hit["product name"],
-        price: hit["sale price"],
-        image: hit["large image url"],
-        category: hit["category 1"],
-        color: hit["color"],
+        objectID: hit['objectID'],
+        name: hit['product name'],
+        price: hit['sale price'],
+        image: hit['large image url'],
+        category: hit['category 1'],
+        color: hit['color'],
         quantity: 1,
-      });
+      })
     }
-    // 
-    // 
+    //
+    //
 
     // Save the updated cart to local storage
-    localStorage.setItem("cart", JSON.stringify(currentCart));
+    localStorage.setItem('cart', JSON.stringify(currentCart))
 
     // Display a notification
-    setNotification(`Item ${hit.objectID} added to cart`);
+    setNotification(`Item ${hit.objectID} added to cart`)
     setTimeout(() => {
-      setNotification("");
-    }, 1000);
-  };
+      setNotification('')
+    }, 1000)
+  }
 
   return (
     <article className="flex flex-col items-center p-2 h-full">
       <div className="flex flex-col items-center flex-grow">
-        <Link
-          to={`/product/${encodeURIComponent(hit.objectID)}`}
-          className="flex justify-center w-full"
-        >
+        <Link to={`/product/${encodeURIComponent(hit.objectID)}`} className="flex justify-center w-full">
           {/* ADJUST ATTRIBUTE NAME BELOW */}
-          <img
-            src={hit["large image url"]}
-            alt={hit["product name"]}
-            className="w-1/2 max-w-xs h-auto mb-2 rounded object-cover"
-          />
+          <img src={hit['large image url']} alt={hit['product name']} className="w-1/2 max-w-xs h-auto mb-2 rounded object-cover" />
           {/*  */}
         </Link>
         {/* ADJUST ATTRIBUTE NAME BELOW */}
-        <p className="text-gray-500">{hit["category 1"]}</p>
+        <p className="text-gray-500">{hit['category 1']}</p>
         {/*  */}
 
         {/* ADJUST ATTRIBUTE NAME BELOW */}
         {/* <h1 className="text-xl font-bold mb-2">{hit["product name"]}</h1> */}
-        <Highlight
-          attribute="product name"
-          hit={hit}
-          tagName="h1"
-          className="text-xl h-12 text-center font-bold mb-2 break-words"
-        />
+        <Highlight attribute="product name" hit={hit} className="text-xl h-12 text-center font-bold mb-2 break-words" />
         {/*  */}
       </div>
       <div className="flex flex-col items-center w-full  mt-4 md:mt-6 lg:mt-8">
-
         {/* ADJUST ATTRIBUTE NAME BELOW */}
-        <p className="text-lg text-green-600">${hit["sale price"]}</p>
+        <p className="text-lg text-green-600">${hit['sale price']}</p>
         {/*  */}
         <div className="flex justify-center items-center space-x-2 mt-2">
-          <button
-            className="bg-green-500 text-white text-sm px-2 py-1 rounded"
-            onClick={handleAddToCart}
-          >
+          <button className="bg-green-500 text-white text-sm px-2 py-1 rounded" onClick={handleAddToCart}>
             Add to Cart
           </button>
           {notification && (
@@ -94,14 +76,12 @@ function Hit({ hit, highlight: Highlight }) {
             </div>
           )}
           <Link to={`/product/${encodeURIComponent(hit.objectID)}`}>
-            <button className="bg-gray-500 text-white text-sm px-2 py-1 rounded">
-              View Details
-            </button>
+            <button className="bg-gray-500 text-white text-sm px-2 py-1 rounded">View Details</button>
           </Link>
         </div>
       </div>
     </article>
-  );
+  )
 }
 
-export default Hit;
+export default Hit
